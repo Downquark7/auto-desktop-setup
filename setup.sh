f93f37e38f7eef328bc0dafd869377fc0eec4372
Downquark7 <mail2emn@gmail.com>
2019-12-09 15:47:29 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
-sudo cp terminal /usr/bin/terminal
-sudo cp i3status.conf /etc/i3status.conf
-sudo cp sddm.conf /etc/sddm.conf
-sudo pacman -S sddm scrot dolphin pcmanfm xorg-server xorg-xbacklight xorg-xinit xorg-xmodmap firefox git i3-gaps dmenu network-manager-applet networkmanager mesa intel-ucode openssh base-devel pavucontrol pulseaudio pulseaudio-alsa alsa-utils alsa-firmware kdeconnect lolcat sl go lxappearance nano htop dunst bluez bluez-utils blueman gtk2 gtk3 zsh
+HOMEDIR=/home/enicely/
+cp terminal /usr/bin/terminal
+cp i3status.conf /etc/i3status.conf
+cp sddm.conf /etc/sddm.conf
+pacman -S sddm scrot dolphin pcmanfm xorg-server xorg-xbacklight xorg-xinit xorg-xmodmap firefox git i3-gaps dmenu network-manager-applet networkmanager mesa intel-ucode openssh base-devel pavucontrol pulseaudio pulseaudio-alsa alsa-utils alsa-firmware kdeconnect lolcat sl go lxappearance nano htop dunst bluez bluez-utils blueman gtk2 gtk3 zsh
 chsh -s /usr/bin/zsh
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -13,13 +13,13 @@ cd ..
 yay -S gtop compton-tryone-blackcapcoder-git ocs-url sddm-theme-sugar-candy-git candy-icons-git oh-my-zsh-git
 sudo cp theme.conf /usr/share/sddm/themes/Sugar-Candy/
 sudo cp cat-rain.jpg /usr/share/sddm/themes/Sugar-Candy/Backgrounds/
-cp .Xmodmap ~/.Xmodmap
-cp vncDisplay.sh ~/vncDisplay.sh
-mkdir ~/.config/
-mkdir ~/.config/i3/
-cp config ~/.config/i3/config
-cp compton.conf ~/.config/compton.conf
-mkdir ~/.config/gtk-3.0/
-cp settings.ini ~/.config/gtk-3.0/settings.ini
-cp .xinitrc ~/.xinitrc
-cp .zshrc ~/.zshrc
+cp .Xmodmap $HOMEDIR/.Xmodmap
+cp vncDisplay.sh $HOMEDIR/vncDisplay.sh
+mkdir $HOMEDIR/.config/
+mkdir $HOMEDIR/.config/i3/
+cp config $HOMEDIR/.config/i3/config
+cp compton.conf $HOMEDIR/.config/compton.conf
+mkdir $HOMEDIR/.config/gtk-3.0/
+cp settings.ini $HOMEDIR/.config/gtk-3.0/settings.ini
+cp .xinitrc $HOMEDIR/.xinitrc
+cp .zshrc $HOMEDIR/.zshrc
