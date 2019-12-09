253fcbab214cdbb5bcc25f65f822c3b45c31df63
Downquark7 <mail2emn@gmail.com>
2019-12-09 15:55:29 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
+useradd -m enicely
+usermod -aG wheel enicely
 HOMEDIR=/home/enicely/
 cp terminal /usr/bin/terminal
 cp i3status.conf /etc/i3status.conf
 cp sddm.conf /etc/sddm.conf
-pacman -S sddm scrot dolphin pcmanfm xorg-server xorg-xbacklight xorg-xinit xorg-xmodmap firefox git i3-gaps dmenu network-manager-applet networkmanager mesa intel-ucode openssh base-devel pavucontrol pulseaudio pulseaudio-alsa alsa-utils alsa-firmware kdeconnect lolcat sl go lxappearance nano htop dunst bluez bluez-utils blueman gtk2 gtk3 zsh
-chsh -s /usr/bin/zsh
+pacman -S sddm scrot dolphin pcmanfm xorg-server xorg-xbacklight xorg-xinit xorg-xmodmap firefox git i3-gaps dmenu network-manager-applet networkmanager mesa intel-ucode openssh base-devel pavucontrol pulseaudio pulseaudio-alsa alsa-utils alsa-firmware kdeconnect lolcat sl go lxappearance nano htop dunst bluez bluez-utils blueman gtk2 gtk3 zsh sudo
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
 #install compton-tryone-git [aur] if animations suck
 yay -S gtop compton-tryone-blackcapcoder-git ocs-url sddm-theme-sugar-candy-git candy-icons-git oh-my-zsh-git
-sudo cp theme.conf /usr/share/sddm/themes/Sugar-Candy/
-sudo cp cat-rain.jpg /usr/share/sddm/themes/Sugar-Candy/Backgrounds/
+cp theme.conf /usr/share/sddm/themes/Sugar-Candy/
+cp cat-rain.jpg /usr/share/sddm/themes/Sugar-Candy/Backgrounds/
 cp .Xmodmap $HOMEDIR/.Xmodmap
 cp vncDisplay.sh $HOMEDIR/vncDisplay.sh
 mkdir $HOMEDIR/.config/
@@ -23,3 +24,4 @@ mkdir $HOMEDIR/.config/gtk-3.0/
 cp settings.ini $HOMEDIR/.config/gtk-3.0/settings.ini
 cp .xinitrc $HOMEDIR/.xinitrc
 cp .zshrc $HOMEDIR/.zshrc
+sudo -u enicely chsh -s /usr/bin/zs
