eee7b8eed2f36a03dc5356dba6aead350216a2ac
Downquark7 <mail2emn@gmail.com>
2019-12-11 13:20:49 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ HOMEDIR=/home/enicely
 cp terminal /usr/bin/terminal
 cp i3status.conf /etc/i3status.conf
 cp sddm.conf /etc/sddm.conf
-pacman -S sddm scrot dolphin pcmanfm xorg-server xorg-xbacklight xorg-xinit xorg-xmodmap firefox git i3-gaps dmenu network-manager-applet networkmanager mesa intel-ucode openssh base-devel pavucontrol pulseaudio pulseaudio-alsa alsa-utils alsa-firmware kdeconnect lolcat sl go lxappearance nano htop dunst bluez bluez-utils blueman gtk2 gtk3 zsh sudo xf86-video-intel i3-status
+cp blurlock /usr/bin/blurlock
+pacman -S sddm scrot dolphin pcmanfm xorg-server xorg-xbacklight xorg-xinit xorg-xmodmap firefox git i3-gaps dmenu network-manager-applet networkmanager mesa intel-ucode openssh base-devel pavucontrol pulseaudio pulseaudio-alsa alsa-utils alsa-firmware kdeconnect lolcat sl go lxappearance nano htop dunst bluez bluez-utils blueman gtk2 gtk3 zsh sudo xf86-video-intel i3-status imagemagick
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
